12a2a5c3b10fa043c22d9d6f0c668d7d461dca95
PetarKing <petarnedeljkovic32815@iths.edu.rs>
2017-08-30 09:38:41 +0200

    Add files via upload

---
+++ DeepSARSA_OpenAI-Gym.py
@@ -0,0 +1,148 @@
+import gym
+import pylab
+import random
+import numpy as np
+from collections import deque
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.models import Sequential
+
+EPISODES = 4000
+
+
+# this is DeepSARSA Agent for the GridWorld
+# Utilize Neural Network as q function approximator
+class DeepSARSAgent:
+    def __init__(self, state_size, action_size):
+
+        self.render = False
+        self.state_size = state_size
+        self.action_size = action_size
+
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+
+        self.epsilon = 1.  # exploration
+        self.epsilon_decay = .9999
+        self.epsilon_min = 0.01
+
+        self.batch_size = 64
+        self.train_start = 1000
+        self.memory = deque(maxlen=10000)
+
+        self.model = self.build_model()
+
+        self.target_model = self.build_model()
+        self.update_target_model()
+
+    # approximate Q function using Neural Network
+    # state is input and Q Value of each action is output of network
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(32, input_dim=self.state_size, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(16, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(self.action_size, activation='linear', kernel_initializer='he_uniform'))
+        model.summary()
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    # get action from model using epsilon-greedy policy
+    def get_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            # The agent acts randomly
+            return random.randrange(self.action_size)
+        else:
+            # Predict the reward value based on the given state
+            state = np.float32(state)
+            q_values = self.model.predict(state)
+            return np.argmax(q_values[0])
+
+    #Turn into SARSA!
+    def replay_memory(self, state, action, reward, next_state,next_action, done):
+        if action == 2:
+            action = 1
+        self.memory.append((state, action, reward, next_state,next_action,  done))
+        if self.epsilon > self.epsilon_min:
+            self.epsilon -= self.epsilon_decay
+
+    def train_replay(self):
+        if len(self.memory) < self.train_start:
+            return
+        batch_size = min(self.batch_size, len(self.memory))
+        mini_batch = random.sample(self.memory, batch_size)
+
+        update_input = np.zeros((batch_size, self.state_size))
+        update_target = np.zeros((batch_size, self.action_size))
+
+        for i in range(batch_size):
+            state, action, reward, next_state, next_action, done = mini_batch[i]
+            target = self.model.predict(state)[0]
+
+            if done:
+                target[action] = reward
+            else:
+                target[action] = (reward + self.discount_factor *
+                              self.model.predict(next_state)[0][next_action])
+            update_input[i] = state
+            update_target[i] = target
+
+        self.model.fit(update_input, update_target, batch_size=batch_size, epochs=1, verbose=0)
+
+    def load_model(self, name):
+        self.model.load_weights(name)
+
+    def save_model(self, name):
+        self.model.save_weights(name)
+
+if __name__ == "__main__":
+    env = gym.make('MountainCar-v0')
+    state_size = env.observation_space.shape[0]
+    #action_size = env.action_space.n
+    action_size = 2
+    agent = DeepSARSAgent(state_size, action_size)
+
+    global_step = 0
+    scores, episodes = [], []
+
+    for e in range(EPISODES):
+        done = False
+        score = 0
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+
+        fake_action = 0
+
+        action_count = 0
+
+        while not done:
+            if agent.render:
+                env.render()
+
+            # get action for the current state and go one step in environment
+            action = agent.get_action(state)
+            next_state, reward, done, info = env.step(action)
+            next_state = np.reshape(next_state, [1, state_size])
+            next_action = agent.get_action(next_state)
+
+            agent.replay_memory(state, action, reward, next_state, next_action, done)
+
+            agent.train_replay()
+            score += reward
+            state = next_state
+
+            if done:
+                env.reset()
+                agent.update_target_model()
+
+                scores.append(score)
+                episodes.append(e)
+                #pylab.plot(episodes, scores, 'b')
+                #pylab.savefig("./save_graph/MountainCar_DSARSA.png")
+                print("episode:", e, "  score:", score, "  memory length:", len(agent.memory),
+                      "  epsilon:", agent.epsilon)
+
+        #if e % 50 == 0:
+         #    agent.save_model("./save_model/deep_sarsa.h5")
